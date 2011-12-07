f7b8820a0397b0c2661730fc56f3ad770cc51a3f
David Soh <sotong0221@gmail.com>
2011-12-07 16:34:40 +0800

    improve sync.sh include add git-relate commands

--- sync.sh
+++ sync.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo --------------------------------------------
+echo copy scripts
 cp /media/Kindle/SotongDJ/createlist.py ~/github/launchpad-kindle/createlist.py
 cp /media/Kindle/SotongDJ/createlist.sh ~/github/launchpad-kindle/createlist.sh
 cp /media/Kindle/SotongDJ/finance.sh ~/github/launchpad-kindle/finance.sh
@@ -13,10 +15,27 @@ cp /media/Kindle/SotongDJ/try.py ~/github/launchpad-kindle/try.py
 cp /media/Kindle/SotongDJ/changelog.txt ~/github/launchpad-kindle/changelog
 cp /media/Kindle/launchpad/SotongDJ.ini ~/github/launchpad-kindle/launchpad/SotongDJ.ini
 cp /media/Kindle/mplayer/control.sh ~/github/launchpad-kindle/mplayer/control.sh
-cp /media/Kindle/documents/KindleNote.azw2 ~/github/launchpad-kindle/kindlenote/KindleNote.azw2
-cp /media/Kindle/SotongDJ/developer.keystore ~/github/launchpad-kindle/kindlenote/developer.keystore
-cp /media/Kindle/SotongDJ/developer.keystore.txt ~/github/launchpad-kindle/kindlenote/developer.keystore.txt
+## cp /media/Kindle/documents/KindleNote.azw2 ~/github/launchpad-kindle/kindlenote/KindleNote.azw2
+## cp /media/Kindle/SotongDJ/developer.keystore ~/github/launchpad-kindle/kindlenote/developer.keystore
+## cp /media/Kindle/SotongDJ/developer.keystore.txt ~/github/launchpad-kindle/kindlenote/developer.keystore.txt
+echo --------------------------------------------
+echo remove gedit backup files
 rm -f /media/Kindle/SotongDJ/*~
 rm -f ~/github/launchpad-kindle/*~
 rm -f /media/Kindle/launchpad/*~
 rm -f /media/Kindle/mplayer/*~
+echo --------------------------------------------
+echo  Add change
+git add -A
+echo --------------------------------------------
+echo Commit change
+echo ---------------------
+echo Please enter your description of
+echo this commit:
+read commit
+git commit -m "$commit"
+echo --------------------------------------------
+echo Push commit\(s\) to GitHub
+git push origin master
+echo --------------------------------------------
+
