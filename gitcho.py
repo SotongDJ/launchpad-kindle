2a9b68a5365cb61c97abc7be98d3a3d2021d91b7
David Soh <sotong0221@gmail.com>
2011-12-10 00:33:24 +0800

    recommit (forgot to un-desc.)-2nd

--- gitcho.py
+++ gitcho.py
@@ -11,7 +11,7 @@ if sys.argv[1] == "change":
     if not rmnbch==selbch:
         cmd="git "+sys.argv[2]+" "+selbch
         print "->"+cmd
-#        status=os.system(cmd)
+        status=os.system(cmd)
 if sys.argv[1] == "nonch":
     status=os.system("git branch>/tmp/brchtemp")
     for bch in open("/tmp/brchtemp").read().splitlines():
@@ -21,4 +21,4 @@ if sys.argv[1] == "nonch":
             rmnbch=bch
     cmd="git "+sys.argv[2]+" "+sys.argv[3]+" "+rmnbch
     print "->"+cmd
-#    status=os.system(cmd)
+    status=os.system(cmd)
