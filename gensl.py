7aba37db1ed3114d715f4a7642369f57d5494c79
David Soh <sotong0221@gmail.com>
2012-04-15 20:23:39 +0800

    Alpha build of gensl.py. gensl.py is the script of generate selection list, it will replace createlist.sh and filter.py.

--- gensl.py
+++ gensl.py
@@ -1,5 +1,6 @@
 #!/mnt/us/python/bin/python2.6
 import sys
+import os
 ## -----------Change it if different---------
 pythonbin="/mnt/us/python/bin/python"
 strpl="/mnt/us/mplayer/playlist"
@@ -12,8 +13,32 @@ forpledit=notepaddir+"/01-Playlist.txt"
 forrecdit=notepaddir+"/02-Reclist.txt"
 forstrdit=notepaddir+"/03-Strlist.txt"
 forsdpldit=notepaddir+"/04-Playlists.txt"
+## ----------------------------------------------
 rectemp=tempdir+"/rectemp"
 playtemp=tempdir+"/playtemp"
 strtemp=tempdir+"/strtemp"
 sdpltemp=tempdir+"/sdpltemp"
 listtemp=tempdir+"/listtemp"
+## ----------------------------------------------
+## Define function
+## ----------------------------------------------
+def arrange():
+    
+    
+    
+    
+## ----------------------------------------------
+## Order
+## ----------------------------------------------
+if "--" in sys.argv[-1]:
+    if "play" in sys.argv[-1]:
+        status=os.system("ls -1 "+music+" > "+playtemp)
+    if "playlist" in sys.argv[-1]:
+        status=os.system("ls -1 "+music+" > "+playtemp)
+##    if "playlists" in sys.argv[-1]:
+##        delay the development of this function
+    if "reclist" in sys.argv[-1]:
+        status=os.system("ls -1 "+record+" > "+rectemp)
+    if "strlist" in sys.argv[-1]:
+        status=os.system("ls -1 "+strpl+" > "+strtemp)
+
