ad24229ae2f6fbe69a9112a608ec22bc8e4fb8f1
David Soh <sotong0221@gmail.com>
2012-04-15 19:18:43 +0800

    start "Rebuild the script set" work

---
+++ gensl.py
@@ -0,0 +1,19 @@
+#!/mnt/us/python/bin/python2.6
+import sys
+## -----------Change it if different---------
+pythonbin="/mnt/us/python/bin/python"
+strpl="/mnt/us/mplayer/playlist"
+tempdir="/mnt/us/SotongDJ"
+notepaddir="/mnt/us/.active-content-data/8a5982e82ae68fb2012bc688405e0026/work/user"
+music="/mnt/us/music"
+record="/mnt/us/record"
+## ----------------------------------------------
+forpledit=notepaddir+"/01-Playlist.txt"
+forrecdit=notepaddir+"/02-Reclist.txt"
+forstrdit=notepaddir+"/03-Strlist.txt"
+forsdpldit=notepaddir+"/04-Playlists.txt"
+rectemp=tempdir+"/rectemp"
+playtemp=tempdir+"/playtemp"
+strtemp=tempdir+"/strtemp"
+sdpltemp=tempdir+"/sdpltemp"
+listtemp=tempdir+"/listtemp"
