bfcf639eb7dc55dc0ad33551bbd45f6cfd152fbe
David Soh <sotong0221@gmail.com>
2012-04-29 12:18:07 +0800

    Remove something no longer use

--- gensl.py
+++ gensl.py
@@ -8,12 +8,10 @@ temp="/tmp/filelisttemp"
 ## ---------------source folder-------------------------------
 musicdir="/mnt/us/music"
 recorddir="/mnt/us/record"
-#strpldir="/mnt/us/music/playlist"
 ## ---------------list file head-------------------------------
 forpledit=notepaddir+"/01-Playlist"
 forrecdit=notepaddir+"/02-Reclist"
 forstrdit=notepaddir+"/03-Strlist"
-#forsdpldit=notepaddir+"/04-Playlists.txt"
 ## ---------------temporary method-------------------------------
 letterset=[['0','1','2','3','4','5','6','7','8','9'],['A','a'],['B','b'],['C','c'],['D','d'],['E','e'],['F','f'],['G','g'],['H','h'],['I','i'],['J','j'],['K','k'],['L','l'],['M','m'],['N','n'],['O','o'],['P','p'],['Q','q'],['R','r'],['S','s'],['T','t'],['U','u'],['V','v'],['W','w'],['X','x'],['Y','y'],['Z'],'Other']
 ## ----------------------------------------------
