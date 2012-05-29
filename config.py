3b947e8891ba8fddba67580ac374495f764056ee
David Soh <sotong0221@gmail.com>
2012-05-29 18:47:00 +0800

    [29052012001] prepare merging testing to master and stop dev as mine kindle damaged

--- config.py
+++ config.py
@@ -107,12 +107,12 @@ def words(thing):
     select=viewvalue('env','kindlenote',configfile)
     if select == 'chinese':
         word01="## 去掉选项前的“不要”即可开启之，反之亦然。\n## 【不要全部播放】 【不要随机播放】\n##（注：重复模式默认开启，目前仍不能选择关闭。）"
-        selword01="\n##\n## m3u 控制选项：\n## 【不要m3u】\n## （需不需要支持m3u播放列表播放。）\n## 注：\n##   当您同时选择开启m3u和随机播放时，\n##	  m3u列表里的所有歌曲会和其他歌曲一起乱序播放。\n##\n## 【不要pl2m3u:NAME】\n## （需不需要把您要播放的媒体编成m3u播放列表,\n##  以NAME.m3u为名，如未更改NAME，将自动以时间日期命名）\n"
+        selword01="\n##\n## m3u 控制选项：\n## 【不要m3u】\n## （需不需要支持m3u播放列表播放。）\n## 注：\n##   当您同时选择开启m3u和随机播放时，\n##	  m3u列表里的所有歌曲会和其他歌曲一起乱序播放。\n"##\n## 【不要pl2m3u:NAME】\n## （需不需要把您要播放的媒体编成m3u播放列表,\n##  以NAME.m3u为名，如未更改NAME，将自动以时间日期命名）\n"
         word02="## 移除“"+nonselectstate+"”以选择您要播放的媒体（如歌曲及录音）。\n"
         word03="## 移除“"+nonselectstate+"”以选择您要播放的播放列表。\n"
     else:
         word01="## Select the mode below by remove \'!\', vice versa\n## (repeat mode is enabled by default)\n## :!playall: :!shuffle:"# :repeat:"
-        selword01="\n##\n## m3u Control Section:\n## :!m3u: (m3u Playlist support)\n## (If you enable Shuffle and m3u at same time,\n##	 the songs in m3u will be arrange ramdomly)\n##\n## :!pl2m3u:NAME: \n## (turn your choice(s) into a m3u playlist\n##  which name is \"NAME.m3u\", if \"NAME\" remain,\n## the name will be \"yymmddhhmm.m3u\")"
+        selword01="\n##\n## m3u Control Section:\n## :!m3u: (m3u Playlist support)\n## (If you enable Shuffle and m3u at same time,\n##	 the songs in m3u will be arrange ramdomly)\n##\n"## :!pl2m3u:NAME: \n## (turn your choice(s) into a m3u playlist\n##  which name is \"NAME.m3u\", if \"NAME\" remain,\n## the name will be \"yymmddhhmm.m3u\")"
         word02="##Select the"+thing+"(s) you want to play by remove \'"+nonselectstate+"\'\n"
         word03="##Select the m3u playlist(s) you want to play by remove \'"+nonselectstate+"\'\n"
     return {'word01':word01,'selword01':selword01,'word02':word02,'word03':word03}
