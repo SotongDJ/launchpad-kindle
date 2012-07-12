0a3c7e5adeb6b5e934652a918f3d85f2d4b27154
David Soh <sotong0221@gmail.com>
2012-07-13 03:23:37 +0800

    [13072012001] add more notif. and func. Fix something

--- system.py
+++ system.py
@@ -1,12 +1,13 @@
 #!/mnt/us/python/bin/python2.6
 import sys
+import os
 ## -----------Change it if different---------
 ## ----------------------------------------------
-o=open("/mnt/us/DK_System/xKindle/config.ini","a")
 ##cmd="/test/bin/usbnetwork"
 ##notif="/mnt/us/mplayer/mplayer /mnt/us/SLoDK/audio/usbNetworkTips.ogg"
 
 if sys.argv[1] == "kindle":
+    o=open("/mnt/us/DK_System/xKindle/config.ini","a")
     for line in open("/mnt/us/DK_System/xKindle/configbk").read().splitlines():
         if  "DefaultSystem" in line:
             o.write("DefaultSystem=3\n")
@@ -15,6 +16,7 @@ if sys.argv[1] == "kindle":
             o.write(line)
     o.close
 if sys.argv[1] == "duokan":
+    o=open("/mnt/us/DK_System/xKindle/config.ini","a")
     for line in open("/mnt/us/DK_System/xKindle/configbk").read().splitlines():
         if  "DefaultSystem" in line:
             o.write("DefaultSystem=1\n")
@@ -23,6 +25,24 @@ if sys.argv[1] == "duokan":
             o.write(line)
     o.close
 
+if sys.argv[1] == "chkip":
+	os.system("ifconfig wlan0>/tmp/iptmp") #for kindle
+#    os.system("ifconfig wlan1>/tmp/iptmp") #for my desktop
+    datasets=[]
+    tmpset=[]
+    ip=''
+    datasets=open("/tmp/iptmp").read().splitlines()
+    for dataset in datasets:
+        tmpset=tmpset+dataset.split(" ")
+    for data in tmpset:
+        if "addr:" in data:
+            ip=data.replace("addr:","")
+            #print ip
+            for a in ip:
+                #print a
+                os.system("/mnt/us/mplayer/mplayer /mnt/us/SLoDK/audio/num"+a+".ogg")
+##
+##
 ##if sys.argv[1] == "usbnetwork":
 ##	os.system(cmd)
 ##	os.system(notif)
